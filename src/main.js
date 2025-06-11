02547eef9a7601e8a01517fe8eebdc9fbb213c9a
Alexandre Ferreira <alexandre.x.f@hotmail.com>
2025-06-11 09:50:05 -0400

    feat: add insertion HTML with JS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { app, db, collection, getDocs } from './firebase.js';
+import { app, db, collection, getDocs } from "./firebase.js";
 
 // Conferir se o Firebase foi inicializado
 console.log("Firebase app initialized", app);
@@ -16,4 +16,36 @@ getDocs(Links)
     console.error("Erro ao acessar documentos: ", error);
   });
 
-  
+const containerHTML = document.getElementById("container");
+
+containerHTML.innerHTML = `
+<img
+        id="profile"
+        src="https://github.com/lucasbarque.png"
+        alt="Foto de Perfil"
+      />
+
+      <div id="info">
+        <h1>Lucas Barque</h1>
+        <p id="description">
+          Engenheiro de software experiente com 9 anos de atuação
+        </p>
+      </div>
+
+      <div id="buttons">
+        <a id="btnFace" href="https://www.facebook.com/" target="_blank"
+          >Facebook</a
+        >
+        <a id="btnInsta" href="https://www.instagram.com/" target="_blank"
+          >Instagram</a
+        >
+        <a id="btnYoutube" href="https://www.youtube.com/" target="_blank"
+          >Youtube</a
+        >
+        <!-- <a id="btnYoutube" href="https://www.youtube.com/" target="_blank"
+          >Youtube</a
+        > -->
+      </div>
+
+      <p id="footer">Development By Alexandre Ferreira © 2025</p>
+`;
